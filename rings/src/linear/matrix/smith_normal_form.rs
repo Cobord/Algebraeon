5a6d1108d927fb716d3143fecf08d4909d666874
Cobord <ammar.s.husain@gmail.com>
2025-06-02 16:04:53 -0400

    long line comment

--- rings/src/linear/matrix/smith_normal_form.rs
+++ rings/src/linear/matrix/smith_normal_form.rs
@@ -1,7 +1,9 @@
 use super::*;
 
 impl<RS: BezoutDomainSignature, RSB: BorrowedStructure<RS>> MatrixStructure<RS, RSB> {
-    //return (u, s, v, k) such that self = usv and s is in smith normal form (with diagonal entries their favorite associates) and u, v are invertible and k is the number of non-zero elements in the diagonal of s
+    //return (u, s, v, k) such that self = usv
+    //and s is in smith normal form (with diagonal entries their favorite associates)
+    //and u, v are invertible and k is the number of non-zero elements in the diagonal of s
     pub fn smith_algorithm(
         &self,
         mut m: Matrix<RS::Set>,
